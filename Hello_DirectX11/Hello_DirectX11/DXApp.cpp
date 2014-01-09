d934bf0702a1c2755eea507a22090c4db22d4223
Noah-Huppert <noahhuppert@gmail.com>
2014-01-08 21:48:46 -0500

    DirectX11 on tut 01
    Got to the point where I am 6 minutes into DirectX11 tut.

--- Hello_DirectX11/Hello_DirectX11/DXApp.cpp
+++ Hello_DirectX11/Hello_DirectX11/DXApp.cpp
@@ -1,15 +1,147 @@
 #include "DXApp.h"
 
+namespace{
+	//Used to forward messages to user def proc function
+	DXApp* g_pApp = nullptr;
+}
+
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+	if (g_pApp){
+		return g_pApp->MsgProc(hwnd, msg, wParam, lParam);
+	}
+	else{
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+}
 
 DXApp::DXApp(HINSTANCE hInstance)
 {
 	m_hAppInstance = hInstance;
 	m_hAppWnd = NULL;
 	m_ClientWidth = 800;
-	m_ClientHeight = 600
+	m_ClientHeight = 600;
+	m_AppTitle = "DirectX11 App";
+	m_WndStyle = WS_OVERLAPPEDWINDOW;
+	g_pApp = this;
+
+	m_pDevice = nullptr;
+	m_pImmediateContext = nullptr;
+	m_pSwapChain = nullptr;
+	m_pRenderTargetView = nullptr;
 }
 
 
 DXApp::~DXApp()
 {
+
+}
+
+int DXApp::Run(){
+	//Main msg loop
+	MSG msg = { 0 };
+
+	while (WM_QUIT != msg.message){
+		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)){
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else{
+			//Update
+			Update(0.0f);
+			//Render
+			Render(0.0f);
+		}
+	}
+
+	return static_cast<int>(msg.wParam);
+}
+
+bool DXApp::Init(){
+	if (!InitWindow()){
+		return false;
+	}
+
+	return true;
+}
+
+bool DXApp::InitWindow(){
+	//Create window class
+	WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(WNDCLASSEX));
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.hInstance = m_hAppInstance;
+	wcex.lpfnWndProc = MainWndProc;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = "Hello_DirectX11";
+	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex)){
+		OutputDebugString("\nFailed to create window class\n");
+		return false;
+	}
+
+	//Adjust height to acomidate top bar
+	RECT r = { 0, 0, m_ClientWidth, m_ClientHeight };
+	AdjustWindowRect(&r, m_WndStyle, FALSE);
+	UINT width = r.right - r.left;
+	UINT height = r.bottom - r.top;
+
+	//Center window
+	UINT x = GetSystemMetrics(SM_CXSCREEN) / 2 - width / 2;
+	UINT y = GetSystemMetrics(SM_CYSCREEN) / 2 - height / 2;
+
+	m_hAppWnd = CreateWindow(
+		"Hello_DirectX11",
+		m_AppTitle.c_str(),
+		m_WndStyle,
+		x,
+		y,
+		width,
+		height,
+		NULL,
+		NULL,
+		m_hAppInstance,
+		NULL);
+		
+	if (!m_hAppWnd){
+		OutputDebugString("\nFailed to create window\n");
+		return false;
+	}
+
+	ShowWindow(m_hAppWnd, SW_SHOW);
+
+	return true;
+}
+
+bool DXApp::InitDirect3D(){
+	UINT createDeviceFlags = 0;
+
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif // _DEBUG
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE
+	};
+
+}
+
+LRESULT DXApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+	switch(msg){
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
 }
